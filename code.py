ca0018b4f86363e7e245d8625d256be173c07501
Paul Beaudet <inof8or@gmail.com>
2017-12-19 17:42:25 -0500

    get ball to move around in a circle

--- code.py
+++ code.py
@@ -3,7 +3,7 @@ import board
 import neopixel
 import time
 
-class Colors:
+class LED:
     def __init__(self):
         self.RED    = (0x10, 0, 0)
         self.YELLOW = (0x10, 0x10, 0)
@@ -12,12 +12,25 @@ class Colors:
         self.BLUE   = (0, 0, 0x10)
         self.PURPLE = (0x10, 0, 0x10)
         self.BLACK  = (0, 0, 0)
+        self.NUMBEROF = 10
 
-color = Colors();                                             # instantiate our color constants
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=.2) # setup and array of neopixels
-pixels.fill(color.BLUE)                                       # Sets all pixels in array to x color
-pixels.show()                                                 # instantiate recorded changes
-time.sleep(9)
-pixels.fill(color.AQUA)
-pixels.show()
- 
+led = LED();                                                            # instantiate our color constants
+pixels = neopixel.NeoPixel(board.NEOPIXEL, led.NUMBEROF, brightness=.2) # setup and array of neopixels
+pixels.fill(led.BLACK)                                                  # Sets all pixels in array to x color
+pixels.show()                                                           # instantiate recorded changes
+
+class Ball:
+    def __init__(self):
+        self.position = led.NUMBEROF
+    def roll(self, delay):
+        self.position = self.position - 1
+        pixels.fill(led.BLACK)                                         # Sets all pixels in array to x color
+        pixels[self.position] = led.BLUE
+        pixels.show()
+        time.sleep(delay)
+        if not self.position:
+           self.position = led.NUMBEROF
+
+pongball = Ball()
+while True:
+    pongball.roll(.001)
