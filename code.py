eb85099f0dc210984bb1b0fe34458ac2f554e543
Paul Beaudet <inof8or@gmail.com>
2017-12-21 11:58:39 -0500

    provide ability for ball to move in both directions

--- code.py
+++ code.py
@@ -40,16 +40,30 @@ class Ball:
         self.position = led.NUMBEROF
         self.timer = JSTimer()
         self.startSpeed = startSpeed
+        self.clockwise = True
     def roll(self):
-        self.position = self.position - 1
-        pixels.fill(led.BLACK)                                         # Sets all pixels in array to x color
-        pixels[self.position] = led.BLUE
-        pixels.show()
-        if not self.position:
-           self.position = led.NUMBEROF
-        self.timer.setTimeout(self.roll, self.startSpeed)
+        pixels.fill(led.BLACK)                 # Sets all pixels in array to x color, removes last ball frame
+        if self.clockwise:                     # given that ball is moving in clockwise direction
+            self.position = self.position - 1  # clockwise is moving backwards through our led array
+            print(self.position)
+            pixels[self.position] = led.BLUE   # set the led in our array
+            pixels.show()                      # this instantiates led to actually light up
+            if not self.position:              # given that we have got to the begining of our array
+                self.position = led.NUMBEROF   # go back to end of array
+        else:                                  # if direction has been set to counter-clockwise 
+            self.position = self.position + 1  # set incremental counter-clockwise position
+            if self.position == led.NUMBEROF:  # given we exceeded bounds of our array
+                self.position = 0
+            pixels[self.position] = led.BLUE
+            pixels.show()
+        self.timer.setTimeout(self.roll, self.startSpeed) # set timeout to progress to next frame
+    def deflect(self, vector):
+        if vector is self.position:
+            self.clockwise = not self.clockwise
 
-pongball = Ball(.01)
+pongball = Ball(1)
 pongball.roll()
+# button b is position 7
+# button a is position 2
 while True:
     pongball.timer.checkTimeout()
